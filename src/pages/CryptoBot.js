15be7b347685198f56c331d017755f3e0f9e986e
unknown <amdsyd2000@gmail.com>
2021-08-28 21:35:06 -0400

    computer

--- src/pages/CryptoBot.js
+++ src/pages/CryptoBot.js
@@ -38,7 +38,7 @@ function CryptoBot() {
   }
 
   async function getImage1(coinName) {
-    await fetch('https://b2fc-2607-fea8-d20-873a-e133-1523-a477-aa3.ngrok.io/image1', {
+    await fetch('https://0747-2607-fea8-d20-873a-6599-6e2b-7fb6-a6a9.ngrok.io/image1', {
       method: 'POST',
       body: JSON.stringify({ coin : coinName})
     }).then(validateResponse)
@@ -49,7 +49,7 @@ function CryptoBot() {
   }
 
   async function getImage2(coinName) {
-    await fetch('https://b2fc-2607-fea8-d20-873a-e133-1523-a477-aa3.ngrok.io/image2', {
+    await fetch('https://0747-2607-fea8-d20-873a-6599-6e2b-7fb6-a6a9.ngrok.io/image2', {
       method: 'POST',
       body: JSON.stringify({ coin : coinName})
     }).then(validateResponse)
