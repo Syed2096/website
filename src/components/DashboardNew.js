1f06658ad8a75ed3dc8f87f16fbbc5f9a154121a
ahmed <skills285@gmail.com>
2023-05-22 22:56:40 -0400

    bugs

--- src/components/DashboardNew.js
+++ src/components/DashboardNew.js
@@ -44,6 +44,7 @@ function DashboardNew() {
   color: white;
   text-align: center;
   font-family: Arial;
+  margin-top: 5rem;
   @media screen and (max-width: 768px) {
     margin-top: 5rem;
   }
@@ -63,10 +64,11 @@ const Icon = styled.img`
   border: 2px solid white;
   display: block;
   margin: 0 auto;
+  margin-top: 10rem;
   width: 200px;
   height: 200px;
   @media screen and (max-width: 768px) {
-    margin-top: 10rem;
+    margin-top: 3rem;
     width: 150px;
     height: 150px;
   }
