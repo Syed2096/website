b5c4d9b613cee9217911c2f9b707d2932ff251b3
ahmed <skills285@gmail.com>
2023-05-23 00:07:04 -0400

    plz

--- src/pages/About.js
+++ src/pages/About.js
@@ -27,9 +27,7 @@ function About() {
     <Background>
       <div style={{...styles.card}}>
         <h1 style={{...styles.main}} >About Me</h1>
-      </div>
-      <div style={{...styles.card}}>
-        <p style={{...styles.paragraph}}>
+        <Paragraph>
         Hello, my name is Ahmed! I'm a passionate software engineer with a deep-rooted love for solving complex problems 
         and crafting innovative solutions. As a computer science graduate, I've honed my skills in various topics from data structures and algorithms 
         to compilers. What sets me apart is my unwavering curiosity and eagerness to learn, which drive me to stay 
@@ -37,18 +35,15 @@ function About() {
         and communication skills foster creativity and seamless cooperation. My portfolio showcases a range of projects where I've 
         leveraged agile methodologies and my attention to detail to deliver exceptional results. I invite you to ask me questions 
         to delve deeper into my experiences and discover how I can contribute to your team!
-        </p>
-      </div>
-      <div>
-        <img style={{...styles.logo}} src={logo} alt='Temp' width='200vw' height='200vh'/>
-      </div>
-        <h1 style={{...styles.paragraph, marginTop: "23rem"}} >Notable Courses</h1>
+        </Paragraph>
+        <Image src={logo}/>
+        <Header>Notable Courses</Header>
         <ul style={{...styles.courses, marginTop: "9rem"}}>
           {courses.map((course, index) => (
             <li key={index}>{course}</li>
           ))}
         </ul>
-      
+      </div>
     </Background>
   );
 }
@@ -65,18 +60,6 @@ const styles = {
     transform: 'translate(-50%,-50%)',
     fontFamily: 'Arial',
     zIndex: -1
-  },
-  paragraph: {
-    color: 'white',
-    position: 'absolute',
-    marginLeft: '50%',
-    top: '10rem',
-    transform: 'translate(-50%,-50%)',
-    fontFamily: 'Arial',
-    zIndex: -1,
-    // height: '100%'
-    width: '60%',
-    'text-align': 'center'
   },
   courses: {
     color: 'white',
@@ -100,31 +83,43 @@ const styles = {
     zIndex: -1,
     opacity: 1
   },
-  logo: {
-    position: 'absolute',
-    left: '50%',
-    top: '15rem',
-    transform: 'translate(-50%,0%)',
-    borderRadius: '100%',
-    borderStyle: 'solid',
-    borderColor: 'white'
-  },
-  background: {
-    'background-color': 'black',
-    position: 'fixed',
-    color: 'white',
-    width: '100%',
-    height: '100%',
-    opacity: '85%',
-    zIndex: -2,
-  },
 };
 
 const Background = styled.div`
-background-color: black;
-position: absolute;
-overflow: auto;
-width: 100%;
-height: 100%;
-opacity: 85%;
+  background-color: black;
+  position: absolute;
+  overflow: auto;
+  width: 100%;
+  height: 110%;
+  opacity: 85%;
+`;
+
+const Header = styled.h1`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0.5rem;
+  margin-top: 20rem;
+  color: white;
+  font-family: arial;
+`;
+
+const Image = styled.img`
+  left: 50%;
+  top: 15rem;
+  position: absolute;
+  border-radius: 100%;
+  background-color: white;
+  height: 20vh;
+  width: 20vh;
+  transform: translate(-50%)
+`;
+
+const Paragraph = styled.p`
+  color: white;
+  width: 75%;
+  margin-left: 50%;
+  margin-top: 6rem;
+  text-align: center;
+  transform: translate(-50%)
 `;
